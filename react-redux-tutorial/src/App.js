324effc3c6dccbcec7f9162dcef8e220adc8a385
김성현 <61579430+kim70273@users.noreply.github.com>
2021-09-13 21:08:46 +0900

    리덕스를 이용한 리액트 상태관리 학습.

---
+++ react-redux-tutorial/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CountetContainer from './containers/CounterContainer';
+import TodosContainer from './containers/TodosContainer';
+
+function App() {
+  return (
+    <div>
+      <CountetContainer />
+      <hr />
+      <TodosContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import Counter from '../components/Counter';
+import { decrease, increase } from '../modules/counter';
+
+const CountetContainer = ({ number, increase, decrease }) => {
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+  );
+};
+
+export default connect(
+  (state) => ({
+    number: state.counter.number,
+  }),
+  {
+    increase,
+    decrease,
+  },
+)(CountetContainer);

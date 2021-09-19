7ef6208aacb7ac86576a54a0ac6902556355aad7
김성현 <61579430+kim70273@users.noreply.github.com>
2021-09-20 05:18:26 +0900

    리액트에서 리덕스 사용 복습 완.

--- react-redux-tutorial/src/modules/counter.js
+++ react-redux-tutorial/src/modules/counter.js
@@ -3,6 +3,7 @@ import { createAction, handleActions } from 'redux-actions';
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
+//액션 객체를 리턴해주는 함수
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 

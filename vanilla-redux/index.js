aa4702c3ec3dc0ac7103f124de18a0781cfb8561
김성현 <61579430+kim70273@users.noreply.github.com>
2021-09-20 03:26:45 +0900

    바닐라자바스크립트에서 리덕스 사용 복습 완.

--- vanilla-redux/index.js
+++ vanilla-redux/index.js
@@ -1,70 +1,74 @@
-import { createStore } from 'redux';
+import { createStore } from "redux";
 
-const divToggle = document.querySelector('.toggle');
-const counter = document.querySelector('h1');
-const btnIncrease = document.querySelector('#increase');
-const btnDecrease = document.querySelector('#decrease');
+const divToggle = document.querySelector(".toggle");
+const counter = document.querySelector("h1");
+const btnIncrease = document.querySelector("#increase");
+const btnDecrease = document.querySelector("#decrease");
 
-const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
-const INCREASE = 'INCREASE';
-const DECREASE = 'DECREASE';
+//액션의 이름
+const TOGGLE_SWITCH = "TOGGLE_SWITCH";
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
 
-const toggleSwitch = () => ({type: TOGGLE_SWITCH});
-const increase = difference => ({type: INCREASE, difference});
-const decrease = () => ({type:DECREASE});
+//액션 객체를 생성해주는 함수
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = (difference) => ({ type: INCREASE, difference });
+const decrease = () => ({ type: DECREASE });
 
 const initialState = {
-    toggle: false,
-    counter: 0
+  toggle: false,
+  counter: 0,
 };
 
+//리듀서 함수
 //state가 undefind일 때는 initialState를 기본 값으로 사용
-function reducer(state=initialState, action){
-    //action.type에 따라 다른 작업을 처리함.
-    switch(action.type){
-        case TOGGLE_SWITCH:
-            return {
-                ...state,//불변성 유지를 위해
-                toggle:!state.toggle
-            };
-        case INCREASE:
-            return {
-                ...state,
-                counter: state.counter+action.difference
-            }
-        case DECREASE:
-            return {
-                ...state,
-                counter: state.counter-1
-            }
-        default:
-            return state;
-    }
+function reducer(state = initialState, action) {
+  //action.type에 따라 다른 작업을 처리함.
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state, //불변성 유지를 위해
+        toggle: !state.toggle,
+      };
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + action.difference,
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
 }
 
+//스토어 생성
 const store = createStore(reducer);
 
 const render = () => {
-    const state = store.getState();//현재상태를 불러온다.
-    
-    if(state.toggle){
-        divToggle.classList.add('active');
-    } else {
-        divToggle.classList.remove('active');
-    }
+  const state = store.getState(); //현재상태를 불러온다.
 
-    counter.innerText = state.counter;
-}
+  if (state.toggle) {
+    divToggle.classList.add("active");
+  } else {
+    divToggle.classList.remove("active");
+  }
+
+  counter.innerText = state.counter;
+};
 
 render();
 store.subscribe(render);
 
 divToggle.onclick = () => {
-    store.dispatch(toggleSwitch());
+  store.dispatch(toggleSwitch());
 };
 btnIncrease.onclick = () => {
-    store.dispatch(increase(1));
+  store.dispatch(increase(1));
 };
 btnDecrease.onclick = () => {
-    store.dispatch(decrease());
+  store.dispatch(decrease());
 };
